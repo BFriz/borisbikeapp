b188a492969208cd7c61c27dfc8c48c410d0d756
Ben Frisby <bfrisb@gmail.com>
2015-05-21 16:32:46 +0100

    refactured test created global bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,14 +3,17 @@ require_relative '../lib/bike'
 describe Bike do
   # this is a specific feature or behaviour
   #that we EXPECT to have
+  let(:bike) {Bike.new }
   it 'should not be borken when created' do
-    bike = Bike.new # Instantiate a new instance of the Bike Class
     expect(bike.broken?).to be false
   end
   it "should be able to break" do
-    bike = Bike.new
     bike.break
     expect(bike.broken?).to be true
   end
-
+  it "should be able to be fixed" do
+    bike.break
+    bike.fix
+    expect(bike.broken?).to be false
+  end
 end
